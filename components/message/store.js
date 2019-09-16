f0013aeec50ebc1bda87194384e6eb2fd5d92ad2
Jair Avilés <hi@jairaviles.mx>
2019-09-15 21:05:56 -0700

    Add MongoDb model Update store using mongoose methods

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,24 @@
+const db = require('mongoose');
+const Model = require('./model')
+
+db.Promise = global.Promise;
+db.connect('', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+console.log('[db] Conectado con exito')
+
 const list = [];
 
 const addMessage = (message) => {
-  list.push(message);
+  // list.push(message);
+  const myMessage = new Model(message);
+  myMessage.save();
 }
 
-const getMessages = () => {
-  return list;
+const getMessages = async () => {
+  const messages = await Model.find();
+  return messages;
 }
 
 module.exports = {
