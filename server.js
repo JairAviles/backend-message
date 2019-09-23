ad5baca153cb4eb2af45dbbb8e7e59e4001b811a
Jair Avilés <hi@jairaviles.mx>
2019-09-22 21:47:53 -0700

    Add cors middleware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app)
 
+const cors = require('cors');
 const bodyParser = require('body-parser')
 const socket = require('./socket')
 const db = require('./db')
@@ -10,6 +11,7 @@ const router = require('./network/routes')
 // Define your own Mongodb String chaing connection ;)
 db('');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
