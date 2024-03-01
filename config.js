0eb6f62ac4b4903711a113a5986fdceeb95e5952
Jair Avilés <JairAviles@users.noreply.github.com>
2024-03-01 12:05:12 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 const config = {
-  dbUrL: process.env.DB_URL || '', // Define your own Mongodb String chaing connection ;)
+  dbUrL: process.env.DB_URL || '', // Define your own Mongodb String chain connection ;)
   port : process.env.PORT || 3000,
   host: process.env.HOST || 'http://localhost',
   publicRoute: process.env.PUBLIC_ROUTE || '/app',
-  filesRoute: process.env.FILES_ROUTE || 'files'
+  filesRoute: process.env.FILES_ROUTE || 'files',
+  sgp: 'sgp_3L89du4eEJNZNSx3tsoV2LqCsAgJFTz05Ged4wVpHWchN7t2MMBL6ZMPhi8sbL3x' // This was revoked by GH secrets scanning, so I don't care if you wanna figure out what is it ;)
 }
 
 module.exports = config;
